88e4b499df781f9db7aa60bc4ad92d4b724bc728
Eric Hernandez <herneri@protonmail.com>
2025-06-16 23:38:15 -0500

    feat: cn: Note printing

---
+++ cn.go
@@ -0,0 +1,40 @@
+package main
+
+/*
+	Copyright 2025 Eric Hernandez
+
+	This file is part of cups.
+
+	cups is free software: you can redistribute it and/or modify it under the terms of the GNU General 
+	Public License as published by the Free Software Foundation, either version 3 of the License,
+	or (at your option) any later version.
+
+	cups is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+	the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+	General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along with cups.
+	If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import (
+	"fmt"
+	"os"
+)
+
+var CN_DIR string = os.Getenv("HOME") + "/.local/share/cups/"
+
+func print_note(note_name string) bool {
+	note_text, err := os.ReadFile(note_name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cn: Failed to read file\n")
+		return false
+	}
+
+	fmt.Printf("%s\n", note_text)
+	return true
+}
+
+func main() {
+	// TODO: Write CLI argument handling
+}
